bd8219591a3eb313473e8d1abe27b32e5ee7f4f6
Aitgul <tursinovaaitgul@gmail.com>
2023-02-04 14:51:15 -0600

    fixed

--- tasks/security-group/security.tf
+++ tasks/security-group/security.tf
@@ -9,31 +9,48 @@ resource "aws_security_group" "main" {
   ingress {
     from_port        = 22
     to_port          = 22
-    protocol         = "-1"
-    cidr_blocks      = [0.0.0.0/0]
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
   }
+}
+resource "aws_security_group" "main" {
+  name        = "main"
+  description = "Allow ports inbound traffic"
+
   ingress {
     from_port        = 80
     to_port          = 80
-    protocol         = "-1"
-    cidr_blocks      = [0.0.0.0/0]
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
   }
+}
+resource "aws_security_group" "main" {
+  name        = "main"
+  description = "Allow ports inbound traffic"
   ingress {
     from_port        = 443
     to_port          = 443
-    protocol         = "-1"
-    cidr_blocks      = [0.0.0.0/0]
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
   }
+}
+resource "aws_security_group" "main" {
+  name        = "main"
+  description = "Allow ports inbound traffic"
   ingress {
     from_port        = 3306
     to_port          = 3306
-    protocol         = "-1"
-    cidr_blocks      = [0.0.0.0/0]
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
   }
+}
+resource "aws_security_group" "main" {
+  name        = "main"
+  description = "Allow ports inbound traffic"
   egress {
     from_port   = 0
     to_port     = 0
-    protocol    = "-1"
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
   }
