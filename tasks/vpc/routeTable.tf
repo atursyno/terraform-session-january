67933f5abcb6ef211203d8933438046a959927e2
Aitgul <tursinovaaitgul@gmail.com>
2023-02-07 14:20:59 -0600

    created folders for homework task and testing

--- tasks/vpc/routeTable.tf
+++ tasks/vpc/routeTable.tf
@@ -7,7 +7,7 @@ resource "aws_route_table""public_route_table"{
   }
 }
 
-resource "aws_route""default_route"{
+resource "aws_route""default_public_route"{
     route_table_id = aws_route_table.public_route_table.id
     destination_cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.homework_internet_gateway.id
@@ -57,7 +57,7 @@ resource "aws_route_table_association""private_routetable_3"{
   }
   }
 
-  resource "aws_route""default_route"{
+  resource "aws_route""default_private_route"{
     route_table_id = aws_route_table.private_route_table.id
     gateway_id = aws_nat_gateway.homework_nat_gateway.id
 
