a716e7402598be309fb3aeccff967bafb19d294d
Aitgul <tursinovaaitgul@gmail.com>
2023-02-06 21:41:49 -0600

    created ouput.tf file

--- session-3/outputs.tf
+++ session-3/outputs.tf
@@ -2,7 +2,7 @@ output "instance_ip_address"{
     value = aws_instance.first_ec2.public_ip
     description = "The public ip address of main EC2 Instance"
 }
-output "instance_volume_id"{
-    value = aws_instance.first_ec2.volume_id
+output "instance_state"{
+    value = aws_instance.first_ec2.instance_state
     description = "The public ip address of main EC2 Instance"
 }
