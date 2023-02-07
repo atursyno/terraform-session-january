96ecced6162c8ab191e2bafd735bf62a898af77f
Aitgul <tursinovaaitgul@gmail.com>
2023-02-07 14:46:27 -0600

    fixed vpc availability_zone

--- tasks/vpc/routeTable.tf
+++ tasks/vpc/routeTable.tf
@@ -59,6 +59,7 @@ resource "aws_route_table_association""private_routetable3"{
 
   resource "aws_route""default_private_route"{
     route_table_id = aws_route_table.private_route_table.id
+    destination_cidr_block = "0.0.0.0/0"
     gateway_id = aws_nat_gateway.homework_nat_gateway.id
 
 
