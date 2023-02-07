57aa7bb57983a05e4f091f0c9f519fda2c6652e8
Aitgul <tursinovaaitgul@gmail.com>
2023-02-07 13:59:40 -0600

    created folders for homework task and testing

--- tasks/vpc/NatGateway.tf
+++ tasks/vpc/NatGateway.tf
@@ -1,6 +1,10 @@
+#---------Create a NatGateway in one of the Public Subnet------
+
 resource "aws_nat_gateway""homework_nat_gateway"{
     subnet_id = aws_subnet.public_subnet_name1.id
+    connectivity_type = "private"
+    allocation_id = aws_eip.nat_gateway_eip.id
     tags ={
-        Name = var.nat_gateWay_name.
+        Name = var.nat_gateWay_name
     }
 }
