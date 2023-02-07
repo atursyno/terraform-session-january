61663244c18b92b775ce90590d348bea3eec4da2
Aitgul <tursinovaaitgul@gmail.com>
2023-02-06 21:15:22 -0600

    fixed

--- session-3/ec2.tf
+++ session-3/ec2.tf
@@ -4,6 +4,6 @@ resource "aws_instance" "first_ec2" {
   vpc_security_group_ids = [aws_security_group.main_sg.id]
   tags = {
     Name = var.name
-    Environment = var.dev
+    Environment = var.env
   } 
 }
