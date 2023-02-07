f34c587627711ce8d262f125e142916202e1226e
Aitgul <tursinovaaitgul@gmail.com>
2023-02-06 20:35:34 -0600

    added tags

--- session-3/ec2.tf
+++ session-3/ec2.tf
@@ -2,5 +2,10 @@ resource "aws_instance" "first_ec2" {
   ami = "ami-05bfbece1ed5beb54"
   instance_type = "t2.micro"
   vpc_security_group_ids = [aws_security_group.main_sg.id]
+  tags = {
+    Name = "development"
+    Environtment = "dev"
+
   
+  } 
 }
