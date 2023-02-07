0c9248a41125ca4e18c586a8e10773f94a85068e
Aitgul <tursinovaaitgul@gmail.com>
2023-02-07 14:48:03 -0600

    fixed vpc availability_zone

--- tasks/vpc/NatGateway.tf
+++ tasks/vpc/NatGateway.tf
@@ -3,7 +3,7 @@
 resource "aws_nat_gateway""homework_nat_gateway"{
     subnet_id = aws_subnet.public_subnet_name1.id
     connectivity_type = "private"
-    allocation_id = aws_eip.nat_gateway_eip.id
+    #allocation_id = aws_eip.nat_gateway_eip.id
     tags ={
         Name = var.nat_gateway_name
     }
