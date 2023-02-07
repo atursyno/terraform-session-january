04cfce2ec4fd56d47ad06cac81585dcc23ad8abc
Aitgul <tursinovaaitgul@gmail.com>
2023-02-07 14:38:03 -0600

    fixed vpc availability_zone

--- tasks/vpc/routeTable.tf
+++ tasks/vpc/routeTable.tf
@@ -15,17 +15,17 @@ resource "aws_route""default_public_route"{
 
 
 #-------------Associate subnets to Public Route Table----------
-resource "aws_route_table_association""public_routetable_1"{
+resource "aws_route_table_association""public_routetable1"{
     subnet_id = aws_subnet.public_subnet_name_1.id
     route_table_id = aws_route_table.public_route_table.id
     
 }
-resource "aws_route_table_association""public_routetable_2"{
+resource "aws_route_table_association""public_routetable2"{
     subnet_id = aws_subnet.public_subnet_name_2.id
     route_table_id = aws_route_table.public_route_table.id
     
 }
-resource "aws_route_table_association""public_routetable_3"{
+resource "aws_route_table_association""public_routetable3"{
     subnet_id = aws_subnet.public_subnet_name_3.id
     route_table_id = aws_route_table.public_route_table.id
     
@@ -33,17 +33,17 @@ resource "aws_route_table_association""public_routetable_3"{
 
 
 #-------------Associate subnets to Private Route Table-----------
-resource "aws_route_table_association""private_routetable_1"{
+resource "aws_route_table_association""private_routetable1"{
     subnet_id = aws_subnet.private_subnet_name_1.id
     route_table_id = aws_route_table.private_route_table.id
     
 }
-resource "aws_route_table_association""private_routetable_2"{
+resource "aws_route_table_association""private_routetable2"{
     subnet_id = aws_subnet.private_subnet_name_2.id
     route_table_id = aws_route_table.private_route_table.id
     
 }
-resource "aws_route_table_association""private_routetable_3"{
+resource "aws_route_table_association""private_routetable3"{
     subnet_id = aws_subnet.private_subnet_name_3.id
     route_table_id = aws_route_table.private_route_table.id
     
