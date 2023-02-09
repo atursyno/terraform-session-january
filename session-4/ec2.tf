ce1050731bb8ec1e7c2bc824cf3586655743c36b
Aitgul <tursinovaaitgul@gmail.com>
2023-02-08 20:59:23 -0600

    Created a folder called session-4

--- session-4/ec2.tf
+++ session-4/ec2.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "ec2_class_task1" {
   ami           = data.aws_ami.amazon_linux_2.image_id
   instance_type = var.instance_type
   tags = {
-    Name        = var.env-instance
+    Name        = "${var.env}-instance"
   }
 } 
 
@@ -10,6 +10,6 @@ resource "aws_instance" "ec2_class_task2" {
   ami           = data.aws_ami.amazon_linux_2.image_id
   instance_type = var.instance_type
   tags = {
-    Name        = var.env-frontend-instance
+    Name        = "${var.env}-frontend-instance"
   }
 } 
