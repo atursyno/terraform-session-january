77b3440548a9e47a78e20793f26437ff08555e58
Aitgul <tursinovaaitgul@gmail.com>
2023-02-09 09:42:50 -0600

    test

--- session-4/ec2.tf
+++ session-4/ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "ec2_class_task1" {
   ami           = data.aws_ami.amazon_linux_2.image_id
   instance_type = var.instance_type
-  #vpc_id = data.aws_vpc.vpc_custom.id
+  vpc_id = aws_vpc.myvpc.id
   vpc_security_group_ids = [aws_security_group.main_sg1.id]
   tags = {
     Name        = "${var.env}-instance"
