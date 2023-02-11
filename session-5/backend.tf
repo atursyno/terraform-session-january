eeb6ddc3f54166d79d54dac411dc7e14484a9466
Aitgul <tursinovaaitgul@gmail.com>
2023-02-11 11:41:41 -0600

    added a dynamodb_table lock key

--- session-5/backend.tf
+++ session-5/backend.tf
@@ -3,7 +3,7 @@ terraform {
         bucket = "terraform-session-september-backend-aika"
         region = "us-east-2"
         key = "session-5/terraform.tfstate" # where does terraform need to store your file. Path or Prefix
-        dynamodb_table "terraform-session-sep-state-lock" #Lock Table
+        dynamodb_table = "terraform-session-sep-state-lock" #Lock Table
     }
 }
 
