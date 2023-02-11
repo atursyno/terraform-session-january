cea5967984aa6c5e9d73d87ff1173016fe0a0b17
Aitgul <tursinovaaitgul@gmail.com>
2023-02-11 11:09:52 -0600

    fixed

--- session-5/ec2.tf
+++ session-5/ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "ec2_class_task1" {
   ami           = data.aws_ami.amazon_linux_2.image_id
   instance_type = var.instance_type
-  vpc_security_group_ids = [aws_security_group.main_sg1.id]
+  vpc_security_group_ids = [aws_security_group.main_sg.id]
   tags = {
     Name        = "${var.env}-instance"
     Name1       = format("%s-instance", var.env)
