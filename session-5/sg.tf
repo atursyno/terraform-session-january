08db106a37c00e959f45cd8b3a891acade664ec4
Aitgul <tursinovaaitgul@gmail.com>
2023-02-11 12:40:56 -0600

    created an element

--- session-5/sg.tf
+++ session-5/sg.tf
@@ -2,7 +2,7 @@ resource "aws_security_group" "main_sg" {
   name        = "${var.env}-sg"
   description = format("%s-sg", var.env)
   #vpc_id      = var.vpc_id
-
+}
 
 resource "aws_security_group_rule""ingress"{
     type             = "ingress"
@@ -12,4 +12,11 @@ resource "aws_security_group_rule""ingress"{
     cidr_blocks      = ["0.0.0.0/0"] # list of strings
     security_group_id = aws_security_group.main_sg.id
   }
-}
+  resource "aws_security_group_rule""egress"{
+    type             = "egress"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"  # string
+    cidr_blocks      = ["0.0.0.0/0"] # list of strings
+    security_group_id = aws_security_group.main_sg.id
+  }
