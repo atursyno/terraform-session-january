d9a09ac5f469a1dc762f2dd9cafaf17e22e5f836
Aitgul <tursinovaaitgul@gmail.com>
2023-02-11 12:24:32 -0600

    added a line

--- session-5/ec2.tf
+++ session-5/ec2.tf
@@ -1,4 +1,5 @@
 resource "aws_instance" "ec2_class_task1" {
+  count = 3 # meta argument, help you with replicating the resources
   ami           = data.aws_ami.amazon_linux_2.image_id
   instance_type = var.instance_type
   vpc_security_group_ids = [aws_security_group.main_sg.id]
