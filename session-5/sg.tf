81dff69040158f5de70fd5da17f6b9b2f76447d1
Aitgul <tursinovaaitgul@gmail.com>
2023-02-11 11:06:57 -0600

    created a folder called session-5

---
+++ session-5/sg.tf
@@ -0,0 +1,18 @@
+resource "aws_security_group" "main_sg" {   
+  name        = "${var.env}-sg"
+  description = format("%s-sg", var.env)
+  #vpc_id      = var.vpc_id
+
+  ingress {
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"  # string
+    cidr_blocks      = ["0.0.0.0/0"] # list of strings
+  }
+   egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1" # string # All ports and protocols
+    cidr_blocks      = ["0.0.0.0/0"] 
+  }
+}
