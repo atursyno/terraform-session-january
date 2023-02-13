180f80e8cab74510eb3b4260aaf7ff8c81525b9a
Aitgul <tursinovaaitgul@gmail.com>
2023-02-12 22:21:02 -0600

    declared all undeclared modules

--- tasks/vpcv1.2/natg.tf
+++ tasks/vpcv1.2/natg.tf
@@ -1,9 +1,9 @@
 #---------Create a NatGateway in one of the Public Subnet------
 
 resource "aws_nat_gateway""nat_gateway_v2"{
-    subnet_id = aws_subnet.public_subnet_namev2.id
-    connectivity_type = var.connectevity_type_ng
-    allocation_id = aws_eip.nat_gateway_eip.id
+    subnet_id = aws_subnet.public_subnet_namev2[0].id
+    connectivity_type = "${var.connectevity_types[0]}"
+    allocation_id = aws_eip.eip.id
     tags ={
         Name = "${var.env}-ng"
     }
