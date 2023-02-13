1d2012d1969d056c6f0883227f5137e2cca23340
Aitgul <tursinovaaitgul@gmail.com>
2023-02-12 22:37:15 -0600

    fixed all incorrect tf files

--- tasks/vpcv1.2/pr_subnet.tf
+++ tasks/vpcv1.2/pr_subnet.tf
@@ -2,7 +2,7 @@ resource "aws_subnet" "private_subnet_name1"{
     count  = 3
     vpc_id = aws_vpc.vpc_version2.id
     availability_zone = element(var.availability_zones, count.index)
-    cidr_block = element(var.public_cidr_blocks, count.index)
+    cidr_block = element(var.private_cidr_blocks, count.index)
     tags ={
         Name = "${var.env}-privateSubnet"
     }
