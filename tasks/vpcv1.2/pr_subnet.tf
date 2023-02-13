7920bfe815b378aeff4a4d0f8766079a28448fba
Aitgul <tursinovaaitgul@gmail.com>
2023-02-13 09:56:14 -0600

    added subnet association

--- tasks/vpcv1.2/pr_subnet.tf
+++ tasks/vpcv1.2/pr_subnet.tf
@@ -1,4 +1,4 @@
-resource "aws_subnet" "private_subnet_name1"{
+resource "aws_subnet" "private_subnet_namev2"{
     count  = 3
     vpc_id = aws_vpc.vpc_version2.id
     availability_zone = element(var.availability_zones, count.index)
