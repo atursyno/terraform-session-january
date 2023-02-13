efc391534dd055390f0f0eda7e6dfcb160068c8e
Aitgul <tursinovaaitgul@gmail.com>
2023-02-12 22:22:14 -0600

    fixed all incorrect tf files

--- tasks/vpcv1.2/natg.tf
+++ tasks/vpcv1.2/natg.tf
@@ -3,7 +3,7 @@
 resource "aws_nat_gateway""nat_gateway_v2"{
     subnet_id = aws_subnet.public_subnet_namev2[0].id
     connectivity_type = "${var.connectevity_types[0]}"
-    allocation_id = aws_eip.eip.id
+    allocation_id = aws_eip.nat_gateway_eip.id
     tags ={
         Name = "${var.env}-ng"
     }
