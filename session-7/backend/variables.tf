9f9da91edb633c08888ef59fac303a8e387491c8
Aitgul <tursinovaaitgul@gmail.com>
2023-02-15 21:24:45 -0600

    created a folder called session-7

---
+++ session-7/backend/variables.tf
@@ -0,0 +1,27 @@
+variable "env"{
+    type = string
+    default = "dev"
+}
+variable "team"{
+    type = string
+    default = "devops"
+}
+variable "app"{
+    type = string
+    default = "peanut"
+
+}
+variable "index"{
+    type = string
+    default = "0"
+
+}
+variable "managed_by"{
+    type = string
+    default = "terraform"
+
+}
+variable "owner"{
+    type = string
+    default = "aika"
+}
