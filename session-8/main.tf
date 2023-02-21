8fe6be1f3d372a14ba1d10a8ad8ebb4e6f70ea79
Aitgul <tursinovaaitgul@gmail.com>
2023-02-20 19:47:07 -0600

    updated the tf files

--- session-8/main.tf
+++ session-8/main.tf
@@ -16,13 +16,20 @@ resource "aws_instance" "main" {
     
     }
   }
-  provisioner = "remote-exec"{
+  provisioner = "remote-exec" {
     inline = [
       "sudo yum install httpd -y",
       "sudo systemctl enable httpd",
       "sudo systemctl start httpd",
       "sudo cp /tmp/index.html /var/www/html/index.html"
     ]
+  connection {
+      type = "ssh"
+      user = "ec2-user"
+      host = self.public_ip  # means that resource block on top
+      private_key = file("~/.ssh/id_rsa")
+    
+    }
   }
 }
 
