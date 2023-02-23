d6fe387f9f0a251a49467b26843fcb6256568461
Aitgul <tursinovaaitgul@gmail.com>
2023-02-22 20:53:16 -0600

    created another module

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,6 +1,7 @@
 resource "aws_instance""mainec2"{
     ami = var.ami
     instance_type = var.instance_type
+    vpc_security_group_ids = var.sg
     tags = {
         Name = var.name
         Environment = "${var.name}-intance"
