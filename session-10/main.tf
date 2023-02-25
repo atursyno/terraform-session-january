1f435eda88ae256fee4e35e78fe93132d98e012a
Aitgul <tursinovaaitgul@gmail.com>
2023-02-25 12:42:17 -0600

    created a folder testing-release

--- session-10/main.tf
+++ session-10/main.tf
@@ -1,6 +1,6 @@
 
 module "ec2_sg"{
-    source = "github.com/atursyno/terraform-session-january/modules/ec2"    # Where the child module is
+    source = "github.com/atursyno/terraform-session-january//modules/ec2?ref=v1.0.0"    # Where the child module is
     ##########Variables that we need here###########
     env = "stage"
     ami = "ami-05bfbece1ed5beb54"
