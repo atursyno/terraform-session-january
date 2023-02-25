2d98469f2a42936bc6ce8df53ecddcd65a63e440
Aitgul <tursinovaaitgul@gmail.com>
2023-02-25 11:41:03 -0600

    created a folder called session-10

--- session-10/main.tf
+++ session-10/main.tf
@@ -1,6 +1,6 @@
 
 module "ec2_sg"{
-    source = "github.com/atursyno/terraform-session-january/tree/main/modules/ec2"    # Where the child module is
+    source = "github.com/atursyno/terraform-session-january/main/modules/ec2"    # Where the child module is
     ##########Variables that we need here###########
     env = "stage"
     ami = "ami-05bfbece1ed5beb54"
