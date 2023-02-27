201655de0f7c75770e8116b642359a6c686c25e7
Aitgul <tursinovaaitgul@gmail.com>
2023-02-26 18:49:32 -0600

    creating an ec2 and associating my vpc

--- session-10/main.tf
+++ session-10/main.tf
@@ -6,4 +6,9 @@ module "ec2_sg"{
     ami = "ami-05bfbece1ed5beb54"
     instance_type = "t2.micro"
 }
-
+module "vpc"{
+    source = "terraform-aws-modules/vpc/aws"
+    version = "3.18.0"
+    env = "${var.env}-vpc"
+    
+}
