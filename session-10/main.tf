fcafa37d403955ba39fc42e51478c2bf5f389a48
Aitgul <tursinovaaitgul@gmail.com>
2023-02-26 20:32:06 -0600

    creating an ec2 and associating my vpc

--- session-10/main.tf
+++ session-10/main.tf
@@ -15,10 +15,11 @@ module "vpc"{
     cidr = var.vpc_cidr
 
     azs             = var.vpc_azs
+    #-------
     private_subnets = var.vpc_private_subnets
     public_subnets  = var.vpc_public_subnets
 
-    enable_nat_gateway = var.vpc_enable_nat_gateway
+    #enable_nat_gateway = var.vpc_enable_nat_gateway
 
     tags = var.vpc_tags
     }
--- session-10/variables.tf
+++ session-10/variables.tf
@@ -24,7 +24,7 @@ variable "vpc_private_subnets" {
 variable "vpc_public_subnets" {
   description = "Public subnets for VPC"
   type        = list(string)
-  default     = ["10.0.101.0/24", "10.0.102.0/24"]
+  default     = ["10.0.11.0/24", "10.0.12.0/24"]
 }
 
 variable "vpc_enable_nat_gateway" {
