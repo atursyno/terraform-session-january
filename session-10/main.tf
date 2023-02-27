169e3c29ac6f024a5c443921a50a4098ca034d5b
Aitgul <tursinovaaitgul@gmail.com>
2023-02-26 20:23:34 -0600

    creating an ec2 and associating my vpc

--- session-10/main.tf
+++ session-10/main.tf
@@ -1,30 +1,26 @@
 
-module "ec2_sg"{
-    source = "github.com/atursyno/terraform-session-january//modules/ec2?ref=v1.0.0"    # Where the child module is
-    ##########Variables that we need here###########
-    env = "stage"
-    ami = "ami-05bfbece1ed5beb54"
-    instance_type = "t2.micro"
-}
+#module "ec2_sg"{
+#    source = "github.com/atursyno/terraform-session-january//modules/ec2?ref=v1.0.0"    # Where the child module is
+#   ##########Variables that we need here###########
+#    env = "stage"
+#    ami = "ami-05bfbece1ed5beb54"
+#    instance_type = "t2.micro"
+#}
+#----------Creating vpc using terraform rg--------------
 module "vpc"{
     source = "terraform-aws-modules/vpc/aws"
     version = "3.18.0"
 
-    name = "vpc-using-tf-registry"
-    cidr = "10.0.0.0/16"
-    instance_tenancy = "default"
-    azs = [var.az1, var.az2]
-    public_subnets = ["10.0.1.0./24", "10.0.2.0/24"]
-    private_subnets = ["10.0.11.0./24", "10.0.12.0/24"]
+    name = "${var.env}-myvpc"
+    cidr = var.vpc_cidr
 
-    enable_nat_gateway = true
+    azs             = var.vpc_azs
+    private_subnets = var.vpc_private_subnets
+    public_subnets  = var.vpc_public_subnets
 
-    tags = {
-    Terraform = "true"
-    Environment = "dev"
-  }
-}
+    enable_nat_gateway = var.vpc_enable_nat_gateway
 
-
-    
+    tags = var.vpc_tags
+    }
+        
 
